5ee3e14fd8a1759468483ef435a9336b1584626f
Nicolas Papagna Maldonado <nicolas.papagna@8thlight.com>
2013-09-04 12:49:25 -0300

    Initial commit: created new rails app without ActiveRecord

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtisanBackofficeIntegration::Application.config.secret_token = '1e05b0863429351bd29ac46f52bcc357f54c1d6072e215ba6ea270decca0a97eaeadeaf424064b9605404d094635d89319ae9b34629fc22d451c9558f2dd0446'
